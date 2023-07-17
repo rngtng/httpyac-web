83191953b841200dd12b2bcb4f569f42b6a3596b
Tobias Bielohlawek <Tobias.Bielohlawek@solarisbank.de>
2023-07-17 12:43:42 +0200

    Update

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -12,7 +12,7 @@ const stylesHandler = 'style-loader';
 const config = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
     },
     plugins: [
         // Add your plugins here
